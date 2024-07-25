7e9bd5a01a36747d464860c397ba9b1dea035105
e-gun <egun@antisigma.classics.utoronto.ca>
2024-07-25 10:13:06 -0400

    comments for exported functions

--- placeandtime.go
+++ placeandtime.go
@@ -25,28 +25,31 @@ type PlaceAndTime struct {
 	TZ  string // e.g., "America/New_York"
 }
 
+// GetRomanDateAndTime - 25 Jul 24 10:09 EDT -->  hora IV a. d. vii Quinctilis MMXXIV (in Toronto...)
 func (p *PlaceAndTime) GetRomanDateAndTime() string {
 	// hora VII pridie Nonas Quinctilis MMXXIV
 	return p.GetRomanTime() + " " + p.GetRomanDate()
 }
 
+// GetRomanTime - 10:09 EDT -->  hora IV (in Toronto...)
 func (p *PlaceAndTime) GetRomanTime() string {
 	// hora VII
 	return getromantime(*p)
 }
 
+// GetRomanDate - a. d. vii Quinctilis MMXXIV
 func (p *PlaceAndTime) GetRomanDate() string {
 	// pridie Nonas Quinctilis MMXXIV
 	return getromandate(p.T)
 }
 
-// GetRTRemainder - how long is a daytime hour at p?
+// GetRTRemainder - how long is a daytime hour for this PlaceAndTime? e.g., 1h16m1.583333333s
 func (p *PlaceAndTime) GetLenOfDaytimeHour() time.Duration {
 	// 1h16m1.583333333s
 	return lengthofdaytimehour(lengthofday(*p))
 }
 
-// GetRTRemainder - how much time is left in this Roman hour before the next hour arrives
+// GetRTRemainder - how much time is left in this Roman hour at this PlaceAndTime before the next hour arrives? e.g. 22m26s
 func (p *PlaceAndTime) GetRTRemainder() time.Duration {
 	// 22m26s
 	elapsed := p.GetRTElapsed()
@@ -60,7 +63,7 @@ func (p *PlaceAndTime) GetRTRemainder() time.Duration {
 	return left
 }
 
-// GetRTElapsed - how much time has passed in this Roman hour
+// GetRTElapsed - how much time has passed in this Roman hour at this PlaceAndTime? e.g. 22m26s
 func (p *PlaceAndTime) GetRTElapsed() time.Duration {
 	// 22m26s
 	var h time.Duration
